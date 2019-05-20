7046eceb3074d303fcbfa772f04535e2beac7f88
Nathan Pierce <nathan.pierce@block.one>
2019-05-20 13:17:45 -0400

    centos test fixes for clang/build-essential

--- tests/bash-bats/modules/clang.bash
+++ tests/bash-bats/modules/clang.bash
@@ -10,15 +10,17 @@ load ../helpers/functions
         [[ ! -z $(echo "${output}" | grep "DCMAKE_CXX_COMPILER='c++'") ]] || exit
         [[ ! -z $(echo "${output}" | grep "DCMAKE_C_COMPILER='cc'") ]] || exit
     elif [[ $NAME == "Ubuntu" ]]; then
-        install-package clang BYPASS_DRYRUN 1>/dev/null
+        install-package build-essential BYPASS_DRYRUN 1>/dev/null # ubuntu 18 build-essential will be high enough, 16 won't and has a version < 7
         run bash -c "printf \"y\n%.0s\" {1..100} | ./$SCRIPT_LOCATION"
         ## CLANG already exists (c++/default) (Ubuntu doesn't have clang>7, so we need to make sure it installs Clang 8)
         [[ ! -z $(echo "${output}" | grep "PIN_COMPILER: false") ]] || exit
-        [[ ! -z $(echo "${output}" | grep "Unable to find C++17 support") ]] || exit
-        [[ ! -z $(echo "${output}" | grep "Clang 8 successfully installed") ]] || exit
-        [[ ! -z $(echo "${output}" | grep "$CLANG_ROOT") ]] || exit
+        if [[ $VERSION_ID == "16.04" ]]; then
+            [[ ! -z $(echo "${output}" | grep "Unable to find C++17 support") ]] || exit
+            [[ ! -z $(echo "${output}" | grep "Clang 8 successfully installed") ]] || exit
+            [[ ! -z $(echo "${output}" | grep "$CLANG_ROOT") ]] || exit
+        fi
         ## CLANG
-        uninstall-package clang BYPASS_DRYRUN 1>/dev/null
+        uninstall-package build-essential BYPASS_DRYRUN 1>/dev/null
         run bash -c "./$SCRIPT_LOCATION -y -P"
         [[ ! -z $(echo "${output}" | grep "PIN_COMPILER: true") ]] || exit
         [[ ! -z $(echo "${output}" | grep "Clang 8 successfully installed") ]] || exit
