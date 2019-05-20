ab0454e21594547e8f7809d9d49d59ee0595c7d8
Nathan Pierce <nathan.pierce@block.one>
2019-05-20 15:00:04 -0400

    [skip ci] centos7 removal since it uses devtoolset-7

--- scripts/eosio_build_centos.bash
+++ scripts/eosio_build_centos.bash
@@ -26,8 +26,7 @@ fi
 # Handle clang/compiler
 ensure-compiler
 # Ensure packages exist
-($PIN_COMPILER && $BUILD_CLANG) && EXTRA_DEPS=(gcc-c++,rpm\ -qa)
-ensure-yum-packages "${REPO_ROOT}/scripts/eosio_build_centos7_deps" $(echo ${EXTRA_DEPS[@]})
+ensure-yum-packages "${REPO_ROOT}/scripts/eosio_build_centos7_deps"
 export PYTHON3PATH="/opt/rh/rh-python36"
 if $DRYRUN || [ -d $PYTHON3PATH ]; then
 	echo "${COLOR_CYAN}[Enabling python36]${COLOR_NC}"
